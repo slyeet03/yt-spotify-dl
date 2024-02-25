1bb941328ed35b5142c98d2ec4a611f7930d0737
slyeet03 <slyeet03@gmail.com>
2024-02-25 15:46:29 +0530

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -8,24 +8,23 @@ def menu():
 	#for yt
 	if input_platform == '1':
 		url = input("Enter the Youtube video URL: ")
-		file_type = input("1)mp3\n2)mp4\n->")
-		file_type_choice(url, file_type)
+		file_type_choice(url)
 
 	#for spotify
 	if input_platform == '2':
 		url = input("Enter the Spotify URL: ")
 		url_converted = spotify_to_youtube_link_converter.spotify_to_youtube(url)
-		file_type = input("1)mp3\n2)mp4\n->")
-		file_type_choice(url_converted, file_type)
+		file_type_choice(url_converted)
 
-def file_type_choice(url, file_type_choice):
+def file_type_choice(url):
 	# Output path where the video will be saved
-	output_path_video = "" #put your desired output path
-	output_path_audio = "" #put your desired output path
+	output_path_video = "C:/Users/chitr/Desktop/yt vids"
+	output_path_audio = "C:/Users/chitr/Desktop/yt audio"
+	file_type = input("1)mp3\n2)mp4\n->")
 
-	if file_type_choice == '1':
+	if file_type == '1':
 		youtube_downloader.audio(url, output_path_audio)
-	if file_type_choice == '2':
+	if file_type == '2':
 		youtube_downloader.video(url, output_path_video)
 
 if __name__ == "__main__":
